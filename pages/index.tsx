8639e8a4b87e71673b4cce35b4c7a981bf1e2b31
erhallow <ehallow24@gmail.com>
2022-03-24 13:36:01 -0500

    Update Working Locally

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,6 @@ import axios from 'axios'
 import Link from 'next/link'
 
 import AWS from 'aws-sdk'
-import product_id from "./[product_id]";
 
 const S3_BUCKET = process.env.NEXT_PUBLIC_S3_BUCKET;
 const REGION = process.env.NEXT_PUBLIC_REGION;
@@ -39,17 +38,9 @@ const Home: NextPage<{ products: string[]}> = (props) => {
 	const [price, setPrice] = useState('')
 	const [image, setImage] = useState<File>()
     const ASTRA_URL = process.env.NEXT_PUBLIC_ASTRA_DB_URL;
-    const ASTRA_SESSION_URL = `${process.env.NEXT_PUBLIC_ASTRA_DB_URL}/activity_stream/`
-    const dateTime = new Date().toISOString();
-    const headers = {
-        'X-Cassandra-Token': `${process.env.NEXT_PUBLIC_ASTRA_DB_APPLICATION_TOKEN}`,
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-    }
 
     const addProductToDraft = async () => {
         setIsLoading(true)
-        console.log('Add Product to Draft')
         const uid = v4()
         const url = `${ASTRA_URL}/catalog/`
 		console.log(url)
@@ -61,9 +52,13 @@ const Home: NextPage<{ products: string[]}> = (props) => {
             "product_id": uid,
             "seller_address": account
             }, {
-            headers: headers
-        });
+            headers: {
+                'X-Cassandra-Token': process.env.NEXT_PUBLIC_ASTRA_DB_APPLICATION_TOKEN,
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            }
 
+        });
         const params = {
             ACL: 'public-read',
             Body: image,
@@ -95,41 +90,25 @@ const Home: NextPage<{ products: string[]}> = (props) => {
         console.log(listNft)
     }
 
-
-    // const trackConnectSession = async (dateTime: string, account: string, productId: string, session_message: string=" ", url: string) => {
-    //
-    //     // await activateBrowserWallet();
-    //     console.log("Account: " + account);
-    //     const session_request = await axios.post(url, {
-    //         "timestamp": dateTime,
-    //         "wallet_address": account,
-    //         "product_id": productId,
-    //         "action": session_message
-    //     },{
-    //         headers: headers
-    //     });
-    //     console.log(account + ' tracked!')
-    //  }
-
     return(
-        <Box >
+        <Box>
             <HStack fontFamily={'Work Sans'} px={16} py={8} align={'center'} justify={'space-between'}>
-                <Link href="/">
-                    <Button
-                        rounded={'full'}
-                        bgColor={'blackAlpha.900'}
-                        color={'whiteAlpha.900'}
-                        _hover={{ bgColor: 'gray.700', fontWeight: 'semibold' }}
-                        fontWeight={'normal'}
-                    >DS Commerce
-                    </Button>
-                </Link>
+            <Link href="/">
+            <Button
+                    rounded={'full'}
+                    bgColor={'blackAlpha.900'}
+                    color={'whiteAlpha.900'}
+                    _hover={{ bgColor: 'gray.700', fontWeight: 'semibold' }}
+                    fontWeight={'normal'}
+                >DS Commerce
+                </Button>
+				</Link>
                 {
                     active && account &&
                     <Text>
                         Connected as:
                         <strong>
-                            {` ${account.slice(0,9)}...${account.slice(-9)}`}
+                        {` ${account.slice(0,9)}...${account.slice(-9)}`}
                         </strong>
                     </Text>
                 }
@@ -139,7 +118,6 @@ const Home: NextPage<{ products: string[]}> = (props) => {
                     color={'whiteAlpha.900'}
                     _hover={{ bgColor: 'gray.700', fontWeight: 'semibold' }}
                     fontWeight={'normal'}
-                    // onClick={() => {!active ? trackConnectSession(dateTime, account, "null", "Connect Metamask Clicked", ASTRA_SESSION_URL) : onOpen()}}
                     onClick={() => {!active ? activateBrowserWallet() : onOpen()}}
                 >
                     {
@@ -147,75 +125,43 @@ const Home: NextPage<{ products: string[]}> = (props) => {
                     }
                 </Button>
             </HStack>
-<<<<<<< Updated upstream
-            <Wrap px={16} py={4} fontFamily={'Work Sans'} justify={'normal'}>
+            <Wrap fontFamily={'Work Sans'} justify={'normal'} px={16} py={4}>
             {
                 active && account &&
                 (
                     props &&
                     props.products.map((product: any) => {
                         return(
-                            <WrapItem key={product.product_id} maxW={'64'}>
+
+                            <WrapItem key={product.product_id} maxW={'64'} m={'50px'}>
+
                                 <Stack border={'1px'} borderColor={'blackAlpha.200'} shadow={'1px'} rounded={'xl'}>
                                     <Link href={`/${product.product_id}`}>
-                                    <a><Img 
+                                    <a><Img
                                         src={`${process.env.NEXT_PUBLIC_AWS_S3!}${product.product_id}.jpeg`}  h={48} w={64} rounded={'xl'}/>
                                      </a></Link>
                                     <Stack p={2} spacing={4}>
-=======
-            <Wrap fontFamily={'Work Sans'} justify={'normal'} px={16} py={4} >
-                {
-                    active && account &&
-                    (
-                        props &&
-                        props.products.map((product: any) => {
-                            return(
 
-                                <WrapItem key={product.product_id} maxW={'64'}>
->>>>>>> Stashed changes
-
-                                    <Stack border={'1px'} borderColor={'blackAlpha.200'} shadow={'1px'} rounded={'xl'} >
                                         <Link href={`/${product.product_id}`}>
-<<<<<<< Updated upstream
                                             <a>
-                                                <Heading as='h2' fontWeight={'semibold'} fontSize={'md'} fontFamily={'Rubik'}>{product.name}</Heading>
+                                                <Heading as='h2' color={'blue.600'} fontWeight={'semibold'} fontSize={'lg'} fontFamily={'Rubik'}>{product.name}</Heading>
                                             </a>
                                         </Link>
-                                        <Text>{product.description}</Text>
-                                        <Text>{product.price} USD</Text>
-                                        <Button>
-                                            {
-=======
-                                            <a><Img
-                                                src={`${process.env.NEXT_PUBLIC_AWS_S3!}${product.product_id}.jpeg`}  h={48} w={64} rounded={'xl'}/>
-                                            </a></Link>
-                                        <Stack p={2} spacing={4}>
-
-                                            <Link href={`/${product.product_id}`}>
-                                                <a>
-                                                    <Heading as='h2' color={'blue.600'} fontWeight={'semibold'} fontSize={'lg'} fontFamily={'Rubik'}>{product.name}</Heading>
-                                                </a>
-                                            </Link>
-                                            <Text><strong>Description:</strong><br/> {product.description}</Text>
-                                            <Text><strong>Price:</strong> {product.price} USD</Text>
-                                            <Text>
-                                                <strong>Status:</strong> {
->>>>>>> Stashed changes
+                                        <Text><strong>Description:</strong><br/> {product.description}</Text>
+                                        <Text><strong>Price:</strong> {product.price} USD</Text>
+                                        <Text>
+                                            <strong>Status:</strong> {
                                                 product.product_status === 'DRAFT' ?
-                                                    'List as NFT' : product.product_status
+                                                'List as NFT' : product.product_status
                                             }
-<<<<<<< Updated upstream
-                                        </Button>
-=======
-                                            </Text>
-                                        </Stack>
->>>>>>> Stashed changes
+                                        </Text>
                                     </Stack>
-                                </WrapItem>
-                            )
-                        })
-                    )
-                }
+                                </Stack>
+                            </WrapItem>
+                        )
+                    })
+                )
+            }
             </Wrap>
             <Modal isOpen={isOpen} onClose={onClose}>
                 <ModalOverlay />
@@ -223,27 +169,27 @@ const Home: NextPage<{ products: string[]}> = (props) => {
                     <ModalHeader>Add Product</ModalHeader>
                     <ModalBody>
                         <Stack>
-                            <FormControl isRequired>
-                                <FormLabel htmlFor='title'>Title</FormLabel>
-                                <Input id='title' placeholder='Title' value={name} onChange={(e) => setName(e.target.value)}/>
-                            </FormControl>
-                            <FormControl isRequired>
-                                <FormLabel htmlFor='price'>Price(in $)</FormLabel>
-                                <Input id='price' placeholder='Price' type={'number'} value={price} onChange={(e) => setPrice(e.target.value)}/>
-                            </FormControl>
-                            <FormControl>
-                                <FormLabel htmlFor='desc'>Description</FormLabel>
-                                <Textarea id='desc' placeholder='Description' value={description} onChange={(e) => setDescription(e.target.value)}/>
-                            </FormControl>
-                            <FormControl>
-                                <FormLabel htmlFor='image'>Upload Image</FormLabel>
-                                <Input id='image' placeholder='Description' type='file' onChange={(e) => setImage(e.target.files![0])}/>
-                            </FormControl>
+                        <FormControl isRequired>
+							<FormLabel htmlFor='title'>Title</FormLabel>
+							<Input id='title' placeholder='Title' value={name} onChange={(e) => setName(e.target.value)}/>
+						</FormControl>
+						<FormControl isRequired>
+							<FormLabel htmlFor='price'>Price(in $)</FormLabel>
+							<Input id='price' placeholder='Price' type={'number'} value={price} onChange={(e) => setPrice(e.target.value)}/>
+						</FormControl>
+						<FormControl>
+							<FormLabel htmlFor='desc'>Description</FormLabel>
+							<Textarea id='desc' placeholder='Description' value={description} onChange={(e) => setDescription(e.target.value)}/>
+						</FormControl>
+						<FormControl>
+							<FormLabel htmlFor='image'>Upload Image</FormLabel>
+							<Input id='image' placeholder='Description' type='file' onChange={(e) => setImage(e.target.files![0])}/>
+						</FormControl>
                         </Stack>
                     </ModalBody>
                     <ModalFooter>
                         <HStack>
-                            <Button onClick={addProductToDraft} isLoading={isLoading}>
+						    <Button onClick={addProductToDraft} isLoading={isLoading}>
                                 Add to Draft
                             </Button>
                         </HStack>
