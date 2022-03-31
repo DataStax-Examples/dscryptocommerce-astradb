98e38dfb41e19a5307e96171244df8de7b579528
BaLu <balu@balu.localdomain>
2022-03-31 20:45:53 +0200

    added multiple blockchain networks logic.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
-import { DAppProvider, Config, Mumbai, Polygon, Localhost, Hardhat } from '@usedapp/core'
+import { DAppProvider, Config, Mumbai, Polygon, Localhost, Hardhat, Ropsten, Rinkeby, Kovan } from '@usedapp/core'
 import '@fontsource/work-sans'
 import '@fontsource/rubik'
 
 const web3config: Config = {
-  networks: [Mumbai, Polygon, Localhost, Hardhat]
+  networks: [Mumbai, Polygon, Localhost, Hardhat, Ropsten, Rinkeby, Kovan]
 }
 
 function MyApp({ Component, pageProps }: AppProps) {
